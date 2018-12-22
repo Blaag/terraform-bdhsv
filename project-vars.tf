55088ecf9888c6da0c62231e2d1ec8605846e076
Christopher Schock - OIT <christopher.schock@state.co.us>
2018-12-21 20:51:07 -0700

    new files

---
+++ project-vars.tf
@@ -0,0 +1,15 @@
+variable agency {
+  default = "cdot"
+}
+
+variable project {
+  default = "workbench"
+}
+
+variable az1 {
+  default = "us-east-1c"
+}
+
+variable az2 {
+  default = "us-east-1d"
+}
