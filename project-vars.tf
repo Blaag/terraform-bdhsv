e7a9c1d26ed436e61115357c06907445dbc4a420
Christopher Schock - OIT <christopher.schock@state.co.us>
2019-02-21 14:41:04 -0700

    added support to make changes to multiple accounts at once

--- project-vars.tf
+++ project-vars.tf
@@ -13,3 +13,23 @@ variable az1 {
 variable az2 {
   default = "us-east-1d"
 }
+
+variable az1-secondary {
+  default = "us-west-2a"
+}
+
+variable az2-secondary {
+  default = "us-west-2b"
+}
+
+variable aws_access_key {
+}
+
+variable aws_secret_key {
+}
+
+variable aws_access_key_secondary {
+}
+
+variable aws_secret_key_secondary {
+}
--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-  #access_key = "${var.aws_access_key}"
-  #secret_key = "${var.aws_secret_key}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
   region     = "us-east-1"
+}
+
+provider "aws" {
+  alias = "secondary"
+  access_key = "${var.aws_access_key_secondary}"
+  secret_key = "${var.aws_secret_key_secondary}"
+  region     = "us-west-2"
 }
